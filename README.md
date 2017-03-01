dc381443ae56da4f2c858e938b1e2aabd3c10afa
rusty1s <matthias.fey@tu-dortmund.de>
2017-03-01 14:44:52 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Embedded Graph Convolutional Neural Network
--- embedded_gcnn/laplacian.py
+++ embedded_gcnn/laplacian.py
@@ -37,8 +37,21 @@ def rescale(L, lmax=2):
     return (2 / lmax) * L - sp.eye(L.shape[0])
 
 
-def chebyshev(L, f, k):
-    """Return T_k * f where T_k are the Chebyshev polynomials of order up to k."""
+def chebyshev(L, X, k, normalized=True):
+    """Return T_k * X where T_k are the Chebyshev polynomials of order k."""
+
+    L = rescale(L, lmax(L, normalized))
+
+    Xt = np.empty((k+1, X.shape[0], X.shape[1]), L.dtype)
+
+    Xt[0] = X
+    Xt[1] = L.dot(X)
+
+    for i in xrange(2, k+1):
+        Xt[k] = 2 * L.dot(Xt[i-1]) - Xt[i-2]
+
+    return Xt[k]
+
 
     largest_eigval, _ = sp.linalg.eigsh(laplacian, 1)
     largest_eigval = largest_eigval[0]
@@ -59,5 +72,5 @@ adj = np.array([[0.0, 1.0, 0.0],
                 [0.0, 1.0, 0.0]])
 adj = sp.coo_matrix(adj)
 laplacian = laplacian(adj, normalized=False)
-print(chebyshev_polynomials(laplacian, 3))
+print(chebyshev(laplacian, 3))
 
