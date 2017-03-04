a0d6f0cef08ee09cec96fa013f6ba26175690d70
rusty1s <matthias.fey@tu-dortmund.de>
2017-03-04 22:55:25 +0100

    graph convolution layer

--- lib/model/layer/gcnn.py
+++ lib/model/layer/gcnn.py
@@ -4,24 +4,11 @@ from .layer import Layer
 from .inits import weight_variable, bias_variable
 
 
-def diag(values):
-    n = values.get_shape()[0].value
-    i = tf.reshape(tf.range(0, n), [n, 1])
-    indices = tf.concat([i, i], axis=1)
-    return tf.SparseTensor(tf.cast(indices, tf.int64), values, [n, n])
-
-
-def normalize_adj(adj, dtype=tf.float32):
-    adj = tf.cast(adj, dtype)
-    n = adj.get_shape()[0].value
-    return tf.sparse_add(adj, diag(tf.ones([n], dtype)))
-
-
 class GraphConvolution(Layer):
     def __init__(self,
                  in_channels,
                  out_channels,
-                 weight_stddev,
+                 weight_stddev=0.0,
                  weight_decay=None,
                  bias=False,
                  bias_constant=0.0,
@@ -49,14 +36,9 @@ class GraphConvolution(Layer):
         if self.logging:
             self._log_vars()
 
-    def _call(self, inputs, adjacencies):
-
-        # normaliez adjacencies
-        # calculate degree
-        # inv_sqrt
-
-        outputs = tf.m
-        outputs = inputs
+    def _call(self, inputs, A):
+        X = tf.sparse_tensor_dense_matmul(A, inputs)
+        outputs = tf.sparse_tensor_dense_matmul(X, self.vars['weights'])
 
         if self.bias:
             outputs += self.vars['bias']
