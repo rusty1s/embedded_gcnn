5513fe0bf9a36d53986a82e94bc0d07a98cb6bf7
rusty1s <matthias.fey@tu-dortmund.de>
2017-03-04 14:13:50 +0100

    new files

--- embedded_gcnn/graph.py
+++ embedded_gcnn/graph.py
@@ -18,10 +18,10 @@ def gaussian(A, sigma=1):
 
     if sp.issparse(A):
         A = A.copy()
-        A.data = np.exp(- A.data / (2 * sigma * sigma))
+        A.data = np.exp(-A.data / (2 * sigma * sigma))
         return A
     else:
-        return np.exp(- A / (2 * sigma * sigma))
+        return np.exp(-A / (2 * sigma * sigma))
 
 
 def grid(shape, connectivity=4, dtype=np.float32):
@@ -35,21 +35,21 @@ def grid(shape, connectivity=4, dtype=np.float32):
 
     for i in xrange(0, num_nodes):
         if i % width > 0:  # left node
-            A[i, i-1] = 1
+            A[i, i - 1] = 1
         if i % width < width - 1:  # right node
-            A[i, i+1] = 1
+            A[i, i + 1] = 1
         if i >= width:  # top node
-            A[i, i-width] = 1
+            A[i, i - width] = 1
             if connectivity > 4 and i % width > 0:  # top left node
-                A[i, i-width-1] = 2
+                A[i, i - width - 1] = 2
             if connectivity > 4 and i % width < width - 1:  # top right node
-                A[i, i-width+1] = 2
+                A[i, i - width + 1] = 2
         if i < height * width - width:  # bottom node
-            A[i, i+width] = 1
+            A[i, i + width] = 1
             if connectivity > 4 and i % width > 0:  # bottom left node
-                A[i, i+width-1] = 2
+                A[i, i + width - 1] = 2
             if connectivity > 4 and i % width < width - 1:  # bottom right node
-                A[i, i+width+1] = 2
+                A[i, i + width + 1] = 2
 
     return A
 
@@ -63,7 +63,7 @@ def embedded(points, neighbors, dtype=np.float32):
     for v1, v2 in neighbors:
         p1 = points[v1]
         p2 = points[v2]
-        d = np.abs(p1[0] - p2[0]) ** 2 + np.abs(p1[1] - p2[1]) ** 2
+        d = np.abs(p1[0] - p2[0])**2 + np.abs(p1[1] - p2[1])**2
         A[v1, v2] = d
         A[v2, v1] = d
 
