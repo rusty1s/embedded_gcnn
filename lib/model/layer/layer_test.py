52b2a3b99d9e10c9f086ddcd27008c50739c7215
rusty1s <matthias.fey@tu-dortmund.de>
2017-03-05 15:50:52 +0100

    revert

--- lib/model/layer/layer_test.py
+++ lib/model/layer/layer_test.py
@@ -18,5 +18,5 @@ class LayerTest(tf.test.TestCase):
         self.assertEqual(layer.name, 'layer_2')
 
     def test_call(self):
-        layer = Layer(name='layer', logging=True)
+        layer = Layer(name='layer')
         self.assertRaises(NotImplementedError, layer.__call__, None)
