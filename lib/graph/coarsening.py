c86042691cd13e7889999e64efe9bfbbb2da15ae
rusty1s <matthias.fey@tu-dortmund.de>
2017-03-06 20:11:50 +0100

    normalize cut;

--- lib/graph/coarsening.py
+++ lib/graph/coarsening.py
@@ -1,11 +1,15 @@
+from __future__ import division
+
 from six.moves import xrange
 
 import numpy as np
 import scipy.sparse as sp
 
 
-def coarsen_adj(adj, level=1):
-    rid = np.random.permutation(np.arange(adj.shape[0]))
+def coarsen_adj(adj, level=1, rid=None):
+    if rid is None:
+        rid = np.random.permutation(np.arange(adj.shape[0]))
+
     degree = np.array(adj.sum(axis=1)).flatten()
 
     # Sort by row index.
@@ -13,8 +17,11 @@ def coarsen_adj(adj, level=1):
     perm = np.argsort(rows)
     rows = rows[perm]
     cols = cols[perm]
-    vals = vals[perm]
-    # TODO: fix vals so that it represents w * (1/d + 1/d)
+    vals = np.array(vals[perm], np.float32)
+
+    # Local normalized cut.
+    for i in xrange(vals.shape[0]):
+        vals[i] = vals[i] * (1 / degree[rows[i]] + 1 / degree[cols[i]])
 
     # Get the beginning indices and the count of a new row of rows
     _, rowstart, rowlength = np.unique(
@@ -27,14 +34,21 @@ def coarsen_adj(adj, level=1):
         if clusters[tid] >= 0:  # already marked
             continue
 
-        # start = rowstart[tid]
-        sliced_vals = vals[rowstart[tid]:rowstart[tid]+rowlength[tid]]
+        start = rowstart[tid]
+        end = start + rowlength[tid]
+        sliced_vals = vals[start:end]
+
+        # Randomly sort values of the looked up row.
+        perm = np.random.permutation(np.arange(sliced_vals.shape[0]))
+        sliced_vals = sliced_vals[perm]
         j = np.argmax(sliced_vals)
 
         if sliced_vals[j] > 0:
             # Get neighbor index.
-            neighbor = cols[rowstart[tid]+j]
-            
+            sliced_cols = cols[start:end]
+            sliced_cols = sliced_cols[perm]
+            neighbor = sliced_cols[j]
+
             # Set cluster.
             clusters[tid] = neighbor
             clusters[neighbor] = tid
@@ -42,9 +56,7 @@ def coarsen_adj(adj, level=1):
             # Set edge weights to zero if the contain one of the nodes.
             vals[np.where(np.logical_or(cols == tid, cols == neighbor))] = 0
         else:
-            print(tid)
             # No unmarked neighbor found.
             clusters[tid] = tid
 
     return clusters
-
