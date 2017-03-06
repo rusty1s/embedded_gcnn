3883c265b263517709c68d06c7a3b725c93982fc
rusty1s <matthias.fey@tu-dortmund.de>
2017-03-06 15:16:13 +0100

    nicer grid adj method

--- lib/graph/adjacency.py
+++ lib/graph/adjacency.py
@@ -22,32 +22,6 @@ def invert_adj(m, sigma=1):
         return np.exp(-m / (2 * sigma * sigma))
 
 
-def _grid_adj_4(adj, height, width):
-    for i in xrange(height * width):
-        if i % width > 0:
-            adj[i, i - 1] = 1
-        if i % width < width - 1:
-            adj[i, i + 1] = 1
-        if i >= width:
-            adj[i, i - width] = 1
-        if i < (height - 1) * width:
-            adj[i, i + width] = 1
-    return adj
-
-
-def _grid_adj_8(adj, height, width):
-    for i in xrange(height * width):
-        if i >= width and i % width > 0:
-            adj[i, i - width - 1] = 2
-        if i >= width and i % width < width - 1:
-            adj[i, i - width + 1] = 2
-        if i < (height - 1) * width and i % width > 0:
-            adj[i, i + width - 1] = 2
-        if i < (height - 1) * width and i % width < width - 1:
-            adj[i, i + width + 1] = 2
-    return adj
-
-
 def grid_adj(shape, connectivity=4, dtype=np.float32):
     """Return adjacency matrix of a regular grid."""
 
@@ -62,6 +36,50 @@ def grid_adj(shape, connectivity=4, dtype=np.float32):
 
     if connectivity == 8:
         adj = _grid_adj_8(adj, height, width)
+
+    return adj
+
+
+def _grid_neighbors(v, height, width):
+    """Return whether the node has a top, bottom, left and right neighbor."""
+    top = v >= width
+    bottom = v < (height - 1) * width
+    left = v % width
+    right = v % width < width - 1
+
+    return top, right, bottom, left
+
+
+def _grid_adj_4(adj, height, width):
+    """Add edges to vertical/horizontal nodes on grid adjacency."""
+    for v in xrange(height * width):
+        top, right, bottom, left = _grid_neighbors(v, height, width)
+
+        if top:
+            adj[v, v - width] = 1
+        if right:
+            adj[v, v + 1] = 1
+        if bottom:
+            adj[v, v + width] = 1
+        if left:
+            adj[v, v - 1] = 1
+
+    return adj
+
+
+def _grid_adj_8(adj, height, width):
+    """Add edges to diagonal nodes on grid adjacency."""
+    for v in xrange(height * width):
+        top, right, bottom, left = _grid_neighbors(v, height, width)
+
+        if top and left:
+            adj[v, v - width - 1] = 2
+        if top and right:
+            adj[v, v - width + 1] = 2
+        if bottom and left:
+            adj[v, v + width - 1] = 2
+        if bottom and right:
+            adj[v, v + width + 1] = 2
 
     return adj
 
