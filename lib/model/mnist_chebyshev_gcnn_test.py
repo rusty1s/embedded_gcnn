27044bee2f5d7869617b6fa314327c5163904c7d
rusty1s <matthias.fey@tu-dortmund.de>
2017-03-11 21:09:36 +0100

    fixed linting

--- lib/model/mnist_chebyshev_gcnn_test.py
+++ lib/model/mnist_chebyshev_gcnn_test.py
@@ -1,13 +1,14 @@
 # import numpy as np
 import tensorflow as tf
 
-from .mnist_chebyshev_gcnn import MNISTChebyshevGCNN
+# from .mnist_chebyshev_gcnn import MNISTChebyshevGCNN
 
 
 class MNISTChebyshevGCNNTest(tf.test.TestCase):
     def test_coarsening(self):
-        placeholders = {
-            'features': tf.placeholder(tf.float32, [1, 28 * 28], 'features'),
-            'labels': tf.placeholder(tf.int32, [1], 'labels'),
-            'dropout': tf.placeholder(tf.float32, [], 'dropout'),
-        }
+        pass
+        # placeholders = {
+        #     'features': tf.placeholder(tf.float32, [1, 28 * 28], 'features'),
+        #     'labels': tf.placeholder(tf.int32, [1], 'labels'),
+        #     'dropout': tf.placeholder(tf.float32, [], 'dropout'),
+        # }
