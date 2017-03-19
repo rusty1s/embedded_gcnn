589a04f13d99c592f35d625db67a68ce278bd578
rusty1s <matthias.fey@tu-dortmund.de>
2017-03-19 10:47:22 +0100

    added clustering test

--- lib/graph/clustering_test.py
+++ lib/graph/clustering_test.py
@@ -12,7 +12,6 @@ class ClusteringTest(TestCase):
         adj = [[0, 2, 1, 0], [2, 0, 0, 1], [1, 0, 0, 2], [0, 1, 2, 0]]
         adj = sp.coo_matrix(adj)
         rid = np.array([0, 1, 2, 3])
-
         assert_equal(normalized_cut(adj, rid), [0, 0, 1, 1])
 
         rid = np.array([3, 2, 1, 0])
@@ -23,9 +22,10 @@ class ClusteringTest(TestCase):
                [2, 0, 3, 0, 1], [0, 0, 0, 1, 0]]
         adj = sp.coo_matrix(adj)
         rid = np.array([0, 1, 2, 3, 4])
-
         assert_equal(normalized_cut(adj, rid), [0, 0, 1, 1, 2])
 
         rid = np.array([4, 3, 2, 1, 0])
-
         assert_equal(normalized_cut(adj, rid), [2, 1, 1, 0, 0])
+
+        rid = np.array([1, 0, 4, 2, 3])
+        assert_equal(normalized_cut(adj, rid), [0, 0, 2, 1, 1])
