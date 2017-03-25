b05760f162bfdfdcd85f254262697476945aaee0
rusty1s <matthias.fey@tu-dortmund.de>
2017-03-25 10:57:11 +0100

    feature selection tries

--- lib/segmentation/feature_extraction.py
+++ lib/segmentation/feature_extraction.py
@@ -12,6 +12,7 @@ def feature_extraction(segmentation, image):
     props = regionprops(segmentation, intensity_image)
 
     NUM_FEATURES = 142
+    NUM_FEATURES = 85
     features = np.zeros((len(props), NUM_FEATURES), dtype=np.float32)
 
     for i, prop in enumerate(props):
@@ -38,17 +39,7 @@ def feature_extraction(segmentation, image):
         moments = prop['moments'].flatten()  # 16 values
         moments_central = prop['moments_central'].flatten()  # 16 values
         moments_hu = prop['moments_hu']  # 8 values
-        moments_normalized = prop['moments_normalized'].flatten()  # 16 values,
-        # potential nan values.
-        # TODO: possible zero division
-        weighted_local_centroid = prop['weighted_local_centroid']  # 2 values
         weighted_moments = prop['weighted_moments'].flatten()  # 16 values
-        weighted_moments_central = prop[
-            'weighted_moments_central'].flatten()  # 16 values
-        weighted_moments_hu = prop['weighted_moments_hu']  # 8 values
-        weighted_moments_normalized = prop[
-            'weighted_moments_normalized'].flatten()  # 16 values,
-        # potential nan values.
 
         sliced_image = image[min_row:max_row, min_col:max_col]
         sliced_image = sliced_image[prop['image']]
@@ -63,21 +54,17 @@ def feature_extraction(segmentation, image):
             area, bbox_height, bbox_width, bbox_area, convex_area,
             eccentricity, equivalent_diameter, extent, filled_area,
             major_axis_length, minor_axis_length, diff_intensity,
-            mean_intensity, orientation, perimeter, solidity]
+            mean_intensity, orientation, perimeter, solidity
+        ]
         feature.extend(inertia_tensor)
         feature.extend(inertia_tensor_eigvals)
         feature.extend(local_centroid)
         feature.extend(moments)
         feature.extend(moments_central)
         feature.extend(moments_hu)
-        feature.extend(moments_normalized)
-        feature.extend(weighted_local_centroid)
         feature.extend(weighted_moments)
-        feature.extend(weighted_moments_central)
-        feature.extend(weighted_moments_hu)
-        feature.extend(weighted_moments_normalized)
-        feature.extend([mean_color_r, mean_color_g,
-            mean_color_b, diff_color_r, diff_color_g, diff_color_b])
+        feature.extend([mean_color_r, mean_color_g, mean_color_b])
+        feature.extend([diff_color_r, diff_color_g, diff_color_b])
 
         assert len(feature) == NUM_FEATURES
         features[i] = np.array(feature)
