426d639430c631c135aff9c10b75ba2eb1173562
rusty1s <matthias.fey@tu-dortmund.de>
2017-04-01 09:03:37 +0200

    readded mnist tests

--- lib/datasets/mnist_test.py
+++ lib/datasets/mnist_test.py
@@ -1,22 +1,21 @@
 from unittest import TestCase
-# import os
-# import shutil
+import os
+import shutil
 
-# from .mnist import MNIST
+from .mnist import MNIST
 
 
 class MNISTTest(TestCase):
     def test_read_dataset(self):
-        pass
-        # mnist = MNIST('mnist')
-        # self.assertTrue(os.path.exists('mnist'))
+        mnist = MNIST('/tmp/mnist')
+        self.assertTrue(os.path.exists('/tmp/mnist'))
 
-        # self.assertEqual(mnist.train._data.shape, (55000, 28, 28, 1))
-        # self.assertEqual(mnist.train._labels.shape, (55000, ))
-        # self.assertEqual(mnist.validation._data.shape, (5000, 28, 28, 1))
-        # self.assertEqual(mnist.validation._labels.shape, (5000, ))
-        # self.assertEqual(mnist.test._data.shape, (10000, 28, 28, 1))
-        # self.assertEqual(mnist.test._labels.shape, (10000, ))
+        self.assertEqual(mnist.train._data.shape, (55000, 28, 28, 1))
+        self.assertEqual(mnist.train._labels.shape, (55000, ))
+        self.assertEqual(mnist.validation._data.shape, (5000, 28, 28, 1))
+        self.assertEqual(mnist.validation._labels.shape, (5000, ))
+        self.assertEqual(mnist.test._data.shape, (10000, 28, 28, 1))
+        self.assertEqual(mnist.test._labels.shape, (10000, ))
 
-        # shutil.rmtree('mnist')
-        # self.assertFalse(os.path.exists('mnist'))
+        shutil.rmtree('/tmp/mnist')
+        self.assertFalse(os.path.exists('/tmp/mnist'))
