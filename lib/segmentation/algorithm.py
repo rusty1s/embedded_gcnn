9316964e14d720992d77f816ffdd3ce321135c73
rusty1s <matthias.fey@tu-dortmund.de>
2017-04-03 11:22:57 +0200

    pascal voc superpixel

--- lib/segmentation/algorithm.py
+++ lib/segmentation/algorithm.py
@@ -8,13 +8,13 @@ def slic(image, num_segments, compactness=10, max_iterations=10, sigma=0):
                              sigma)
 
 
-def quickshift(image, ratio=5, kernel_size=5, max_dist=1, sigma=0):
+def quickshift(image, ratio=1, kernel_size=5, max_dist=1, sigma=0):
     image = _preprocess(image)
     return segmentation.quickshift(
         image, ratio, kernel_size, max_dist, sigma=sigma)
 
 
-def felzenszwalb(image, scale=100, min_size=1, sigma=0):
+def felzenszwalb(image, scale=1, min_size=1, sigma=0):
     image = _preprocess(image)
     return segmentation.felzenszwalb(image, scale, sigma, min_size)
 
