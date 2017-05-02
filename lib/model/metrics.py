56d819b396ab52dad6ffb252f8a905df885b67e2
rusty1s <matthias.fey@tu-dortmund.de>
2017-05-02 17:09:44 +0200

    cal accuracy with multi label

--- lib/model/metrics.py
+++ lib/model/metrics.py
@@ -20,16 +20,19 @@ def cal_softmax_cross_entropy(outputs, labels):
 def cal_accuracy(outputs, labels):
     """Calculate accuracy."""
 
+    num_labels = labels.get_shape()[1]
+
     with tf.name_scope('accuracy'):
-        predicted_labels = tf.argmax(outputs, 1)
-        predicted_labels = tf.cast(predicted_labels, tf.int32)
+        predicted_labels = tf.argmax(outputs, axis=1)
 
-        # TODO: Multi label problem
-        correct_labels = tf.argmax(labels, 1)
-        correct_labels = tf.cast(correct_labels, tf.int32)
+        predicted_labels_one_hot = tf.one_hot(predicted_labels, num_labels)
+        predicted_labels_one_hot = tf.cast(predicted_labels_one_hot, tf.bool)
 
-        correct_prediction = tf.equal(predicted_labels, correct_labels)
+        labels = tf.cast(labels, tf.bool)
+        correct_prediction = tf.logical_and(predicted_labels_one_hot, labels)
+
         correct_prediction = tf.cast(correct_prediction, tf.float32)
+        correct_prediction = tf.reduce_max(correct_prediction, axis=1)
 
         accuracy = tf.reduce_mean(correct_prediction)
 
---
+++ lib/model/metrics_test.py
@@ -0,0 +1,49 @@
+import tensorflow as tf
+
+from .metrics import cal_accuracy
+
+
+class MetricsTest(tf.test.TestCase):
+    def test_cal_accuracy(self):
+        outputs = [[0.8, 0.5, 0.3, 0.9], [0.3, 0.4, 0.6, 0.4]]
+        outputs = tf.constant(outputs, tf.float32)
+
+        labels = [[1, 0, 0, 0], [0, 0, 1, 0]]
+        labels = tf.constant(labels, tf.int32)
+
+        with self.test_session():
+            self.assertEqual(cal_accuracy(outputs, labels).eval(), 0.5)
+
+        labels = [[0, 0, 0, 1], [0, 0, 1, 0]]
+        labels = tf.constant(labels, tf.int32)
+
+        with self.test_session():
+            self.assertEqual(cal_accuracy(outputs, labels).eval(), 1)
+
+        labels = [[0, 1, 0, 0], [0, 1, 0, 0]]
+        labels = tf.constant(labels, tf.int32)
+
+        with self.test_session():
+            self.assertEqual(cal_accuracy(outputs, labels).eval(), 0)
+
+    def test_cal_accuracy_multilabel(self):
+        outputs = [[0.8, 0.5, 0.3, 0.9], [0.3, 0.4, 0.6, 0.4]]
+        outputs = tf.constant(outputs, tf.float32)
+
+        labels = [[1, 1, 0, 0], [0, 1, 1, 1]]
+        labels = tf.constant(labels, tf.int32)
+
+        with self.test_session():
+            self.assertEqual(cal_accuracy(outputs, labels).eval(), 0.5)
+
+        labels = [[1, 1, 1, 0], [1, 1, 0, 1]]
+        labels = tf.constant(labels, tf.int32)
+
+        with self.test_session():
+            self.assertEqual(cal_accuracy(outputs, labels).eval(), 0)
+
+        labels = [[0, 1, 1, 1], [1, 0, 1, 0]]
+        labels = tf.constant(labels, tf.int32)
+
+        with self.test_session():
+            self.assertEqual(cal_accuracy(outputs, labels).eval(), 1)
