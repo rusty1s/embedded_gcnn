656cc21a8eded34ea9e768b9b58272dbb5a5ab60
rusty1s <matthias.fey@tu-dortmund.de>
2017-05-04 11:20:47 +0200

    no print capture on travis, so download stops getting aborted

--- lib/datasets/download.py
+++ lib/datasets/download.py
@@ -7,6 +7,14 @@ import tarfile
 from six.moves import urllib
 
 
+_percentage = 0
+
+
+def _print_status(name, percentage):
+    sys.stdout.write('\r>> Downloading {} {:.2f}%'.format(name, percentage))
+    sys.stdout.flush()
+
+
 def maybe_download_and_extract(url, data_dir):
     if not os.path.exists(data_dir):
         os.makedirs(data_dir)
@@ -18,13 +26,17 @@ def maybe_download_and_extract(url, data_dir):
     if not os.path.exists(filepath):
 
         def _progress(count, block_size, total_size):
-            sys.stdout.write(
-                '\r>> Downloading {} {:.2f}%'
-                .format(filename, 100 * count * block_size / total_size))
-            sys.stdout.flush()
+            global _percentage
+            percentage = 100 * count * block_size / total_size
+
+            # Only print if percentages have changed by 1%.
+            if percentage + 1 > _percentage:
+                _print_status(filename, percentage)
+                _percentage = percentage
 
         filepath, _ = urllib.request.urlretrieve(url, filepath, _progress)
         size = os.stat(filepath).st_size
+        _print_status(filename, 100)
 
         print()
         print('Successfully downloaded {} ({} bytes).'.format(filename, size))
