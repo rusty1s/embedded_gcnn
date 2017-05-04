9f9f857f096446f178322c55f245921cf030137e
rusty1s <matthias.fey@tu-dortmund.de>
2017-05-04 16:54:22 +0200

    first impl of better feature extraction

---
+++ lib/segmentation/feature_extraction_new.py
@@ -0,0 +1,334 @@
+from __future__ import division
+
+from cached_property import cached_property
+import numpy as np
+from numpy import pi as PI
+import numpy_groupies as npg
+
+
+class FeatureExtraction(object):
+    def __init__(self, segmentation):
+        self.segmentation = segmentation
+
+    @cached_property
+    def _flat(self):
+        return self.segmentation.flatten()
+
+    @cached_property
+    def _ys(self):
+        height, width = self.segmentation.shape
+        return np.arange(height).repeat(width)
+
+    @cached_property
+    def _xs(self):
+        height, width = self.segmentation.shape
+        return np.tile(np.arange(width), height)
+
+    @cached_property
+    def _ys2(self):
+        return self._ys * self._ys
+
+    @cached_property
+    def _xs2(self):
+        return self._xs * self._xs
+
+    @cached_property
+    def _min_y(self):
+        return npg.aggregate(self._flat, self._ys, func='min')
+
+    @cached_property
+    def _max_y(self):
+        return 1 + npg.aggregate(self._flat, self._ys, func='max')
+
+    @cached_property
+    def _min_x(self):
+        return npg.aggregate(self._flat, self._xs, func='min')
+
+    @cached_property
+    def _max_x(self):
+        return 1 + npg.aggregate(self._flat, self._xs, func='max')
+
+    @cached_property
+    def _M_00(self):
+        ones = np.ones_like(self._flat)
+        return npg.aggregate(self._flat, ones, func='sum')
+
+    @cached_property
+    def _M_01(self):
+        return npg.aggregate(self._flat, self._ys, func='sum')
+
+    @cached_property
+    def _M_10(self):
+        return npg.aggregate(self._flat, self._xs, func='sum')
+
+    @cached_property
+    def _M_11(self):
+        return npg.aggregate(self._flat, self._ys * self._xs, func='sum')
+
+    @cached_property
+    def _M_02(self):
+        return npg.aggregate(self._flat, self._ys2, func='sum')
+
+    @cached_property
+    def _M_20(self):
+        return npg.aggregate(self._flat, self._xs2, func='sum')
+
+    @cached_property
+    def _M_12(self):
+        return npg.aggregate(self._flat, self._ys2 * self._xs, func='sum')
+
+    @cached_property
+    def _M_21(self):
+        return npg.aggregate(self._flat, self._ys * self._xs2, func='sum')
+
+    @cached_property
+    def _M_03(self):
+        return npg.aggregate(self._flat, self._ys2 * self._ys, func='sum')
+
+    @cached_property
+    def _M_30(self):
+        return npg.aggregate(self._flat, self._xs2 * self._xs, func='sum')
+
+    @cached_property
+    def _centroid_y(self):
+        return self._M_01 / self._M_00
+
+    @cached_property
+    def _centroid_x(self):
+        return self._M_10 / self._M_00
+
+    @cached_property
+    def _centroid_y2(self):
+        return self._centroid_y * self._centroid_y
+
+    @cached_property
+    def _centroid_x2(self):
+        return self._centroid_x * self._centroid_x
+
+    @cached_property
+    def _inertia_tensor_eigvals_sum_1(self):
+        return (self.inertia_tensor_20 + self.inertia_tensor_02) / 2
+
+    @cached_property
+    def _inertia_tensor_eigvals_sum_2(self):
+        sum_1 = 4 * self.inertia_tensor_11 * self.inertia_tensor_11
+        sum_2 = (self.inertia_tensor_20 - self.inertia_tensor_02)
+        sum_2 = sum_2 * sum_2
+        return np.sqrt(sum_1 + sum_2) / 2
+
+    def _nu_denominator(self, i, j):
+        return np.power(self._M_00, 1 + (i + j) / 2)
+
+    @cached_property
+    def mu_11(self):
+        return self._M_11 - self._centroid_x * self._M_01
+
+    @cached_property
+    def mu_02(self):
+        return self._M_02 - self._centroid_y * self._M_01
+
+    @cached_property
+    def mu_20(self):
+        return self._M_20 - self._centroid_x * self._M_10
+
+    @cached_property
+    def mu_12(self):
+        sum_1 = self._M_12 - 2 * self._centroid_y * self._M_11
+        sum_2 = self._centroid_x * self._M_02
+        sum_3 = 2 * self._centroid_y2 * self._M_10
+        return sum_1 - sum_2 + sum_3
+
+    @cached_property
+    def mu_21(self):
+        sum_1 = self._M_21 - 2 * self._centroid_x * self._M_11
+        sum_2 = self._centroid_y * self._M_20
+        sum_3 = 2 * self._centroid_x2 * self._M_01
+        return sum_1 - sum_2 + sum_3
+
+    @cached_property
+    def mu_03(self):
+        sum_1 = 3 * self._centroid_y * self._M_02
+        sum_2 = 2 * self._centroid_y2 * self._M_01
+        return self._M_03 - sum_1 + sum_2
+
+    @cached_property
+    def mu_30(self):
+        sum_1 = 3 * self._centroid_x * self._M_20
+        sum_2 = 2 * self._centroid_x2 * self._M_10
+        return self._M_30 - sum_1 + sum_2
+
+    @cached_property
+    def inertia_tensor_02(self):
+        return self.mu_02 / self._M_00
+
+    @cached_property
+    def inertia_tensor_20(self):
+        return self.mu_20 / self._M_00
+
+    @cached_property
+    def inertia_tensor_11(self):
+        return self.mu_11 / self._M_00
+
+    @cached_property
+    def inertia_tensor_eigvals_1(self):
+        a = self._inertia_tensor_eigvals_sum_1
+        b = self._inertia_tensor_eigvals_sum_2
+        return a + b
+
+    @cached_property
+    def inertia_tensor_eigvals_2(self):
+        a = self._inertia_tensor_eigvals_sum_1
+        b = self._inertia_tensor_eigvals_sum_2
+        return a - b
+
+    @cached_property
+    def nu_11(self):
+        return self.mu_11 / self._nu_denominator(1, 1)
+
+    @cached_property
+    def nu_02(self):
+        return self.mu_02 / self._nu_denominator(0, 2)
+
+    @cached_property
+    def nu_20(self):
+        return self.mu_20 / self._nu_denominator(2, 0)
+
+    @cached_property
+    def nu_12(self):
+        return self.mu_12 / self._nu_denominator(1, 2)
+
+    @cached_property
+    def nu_21(self):
+        return self.mu_21 / self._nu_denominator(2, 1)
+
+    @cached_property
+    def nu_03(self):
+        return self.mu_03 / self._nu_denominator(0, 3)
+
+    @cached_property
+    def nu_30(self):
+        return self.mu_30 / self._nu_denominator(3, 0)
+
+    @cached_property
+    def hu_1(self):
+        return self.nu_20 + self.nu_02
+
+    @cached_property
+    def hu_2(self):
+        sum_1 = self.nu_20 - self.nu_02
+        sum_1 = sum_1 * sum_1
+        sum_2 = 2 * self.nu_11
+        sum_2 = sum_2 * sum_2
+        return sum_1 + sum_2
+
+    @cached_property
+    def hu_3(self):
+        sum_1 = self.nu_30 - 3 * self.nu_12
+        sum_1 = sum_1 * sum_1
+        sum_2 = 3 * self.nu_21 - self.nu_03
+        sum_2 = sum_2 * sum_2
+        return sum_1 + sum_2
+
+    @cached_property
+    def hu_4(self):
+        sum_1 = self.nu_30 + self.nu_12
+        sum_1 = sum_1 * sum_1
+        sum_2 = self.nu_21 + self.nu_03
+        sum_2 = sum_2 * sum_2
+        return sum_1 + sum_2
+
+    @cached_property
+    def hu_5(self):
+        prod_1 = (self.nu_30 - 3 * self.nu_12) * (self.nu_30 + self.nu_12)
+        prod_21 = self.nu_30 + self.nu_12
+        prod_21 = prod_21 * prod_21
+        prod_22 = self.nu_21 + self.nu_03
+        prod_22 = 3 * prod_22 * prod_22
+        prod_2 = prod_21 - prod_22
+        sum_1 = prod_1 * prod_2
+        prod_1 = (3 * self.nu_21 - self.nu_03) * (self.nu_21 + self.nu_03)
+        prod_21 = self.nu_30 + self.nu_12
+        prod_21 = 3 * prod_21 * prod_21
+        prod_22 = self.nu_21 + self.nu_03
+        prod_22 = prod_22 * prod_22
+        prod_2 = prod_21 - prod_22
+        sum_2 = prod_1 * prod_2
+        return sum_1 + sum_2
+
+    @cached_property
+    def hu_6(self):
+        raise NotImplementedError
+
+    @cached_property
+    def hu_7(self):
+        raise NotImplementedError
+
+    @cached_property
+    def area(self):
+        return self._M_00
+
+    @cached_property
+    def bbox_height(self):
+        return self._max_y - self._min_y
+
+    @cached_property
+    def bbox_width(self):
+        return self._max_x - self._min_x
+
+    @cached_property
+    def bbox_area(self):
+        return self.bbox_height * self.bbox_width
+
+    @cached_property
+    def centroid_y(self):
+        return self._centroid_y - self._min_y
+
+    @cached_property
+    def centroid_x(self):
+        return self._centroid_x - self._min_x
+
+    @cached_property
+    def convex_area(self):
+        raise NotImplementedError
+
+    @cached_property
+    def eccentricity(self):
+        eigval_1 = self.inertia_tensor_eigvals_1
+        eigval_2 = self.inertia_tensor_eigvals_2
+        return np.sqrt(1 - eigval_2 / eigval_1)
+
+    @cached_property
+    def equivalent_diameter(self):
+        return np.sqrt(4 * self._M_00 / PI)
+
+    @cached_property
+    def euler_number(self):
+        raise NotImplementedError
+
+    @cached_property
+    def extent(self):
+        return self._M_00 / self.bbox_area
+
+    @cached_property
+    def filled_area(self):
+        raise NotImplementedError
+
+    @cached_property
+    def major_axis_length(self):
+        return 4 * np.sqrt(self.inertia_tensor_eigvals_1)
+
+    @cached_property
+    def minor_axis_length(self):
+        return 4 * np.sqrt(self.inertia_tensor_eigvals_2)
+
+    @cached_property
+    def orientation(self):
+        raise NotImplementedError
+
+    @cached_property
+    def perimeter(self):
+        raise NotImplementedError
+
+    @cached_property
+    def solidity(self):
+        raise NotImplementedError
--- lib/segmentation/feature_extraction_test.py
+++ lib/segmentation/feature_extraction_test.py
@@ -1,21 +1,125 @@
 from unittest import TestCase
 
-# import numpy as np
-# from numpy.testing import assert_equal
+import numpy as np
+from skimage.measure import regionprops
+from numpy.testing import assert_equal, assert_almost_equal
 
-# from .feature_extraction import feature_extraction
+from .feature_extraction_new import FeatureExtraction
+
+
+def _convert(props, key):
+    p = np.array([prop[key] for prop in props])
+    if (p.ndim > 0):
+        p = p.T
+    return p
 
 
 class FeatureExtractionTest(TestCase):
     def test_feature_extraction_minimal(self):
-        return
-        # segmentation = np.array([[0, 1, 1], [0, 1, 1]])
-        # image = np.array([[[0, 0, 0], [1, 1, 1], [1, 1, 1]],
-        #                   [[0, 0, 0], [1, 1, 1], [1, 1, 1]]], np.float32)
+        segmentation = np.array([[0, 0, 1, 1], [0, 0, 1, 1]])
 
-        # features = feature_extraction_minimal(segmentation, image)
+        features = FeatureExtraction(segmentation)
+        props = regionprops(segmentation + 1)
 
-        # expected = [[2, 2, 1, 0], [4, 2, 2, 1]]
+        # Test private helper
+        assert_equal(features._min_y, [0, 0])
+        assert_equal(features._max_y, [2, 2])
+        assert_equal(features._min_x, [0, 2])
+        assert_equal(features._max_x, [2, 4])
 
-        # assert_equal(features.shape, (2, 4))
-        # assert_equal(features, expected)
+        assert_equal(features._M_00, [4, 4])
+        assert_equal(features._M_01, [2, 2])
+        assert_equal(features._M_10, [2, 10])
+        assert_equal(features._M_11, [1, 5])
+        assert_equal(features._M_02, [2, 2])
+        assert_equal(features._M_20, [2, 26])
+        assert_equal(features._M_12, [1, 5])
+        assert_equal(features._M_21, [1, 13])
+        assert_equal(features._M_03, [2, 2])
+        assert_equal(features._M_30, [2, 70])
+
+        assert_equal(features._centroid_y, _convert(props, 'centroid')[0])
+        assert_equal(features._centroid_x, _convert(props, 'centroid')[1])
+
+        # Test central moments.
+        moments_central = _convert(props, 'moments_central')
+        assert_equal(features.mu_11, moments_central[1, 1])
+        assert_equal(features.mu_02, moments_central[2, 0])
+        assert_equal(features.mu_20, moments_central[0, 2])
+        assert_equal(features.mu_12, moments_central[2, 1])
+        assert_equal(features.mu_21, moments_central[1, 2])
+        assert_equal(features.mu_03, moments_central[3, 0])
+        assert_equal(features.mu_30, moments_central[0, 3])
+
+        inertia_tensor = _convert(props, 'inertia_tensor')
+        assert_equal(features.inertia_tensor_02, inertia_tensor[1, 1])
+        assert_equal(features.inertia_tensor_20, inertia_tensor[0, 0])
+        assert_equal(features.inertia_tensor_11, inertia_tensor[1, 0])
+        assert_equal(features.inertia_tensor_11, inertia_tensor[0, 1])
+
+        eigvals = _convert(props, 'inertia_tensor_eigvals')
+        assert_equal(features.inertia_tensor_eigvals_1, eigvals[0])
+        assert_equal(features.inertia_tensor_eigvals_2, eigvals[1])
+
+        # Tests normalized moments.
+        moments_normalized = _convert(props, 'moments_normalized')
+        assert_equal(features.nu_11, moments_normalized[1, 1])
+        assert_equal(features.nu_02, moments_normalized[2, 0])
+        assert_equal(features.nu_20, moments_normalized[0, 2])
+        assert_equal(features.nu_12, moments_normalized[2, 1])
+        assert_equal(features.nu_21, moments_normalized[1, 2])
+        assert_equal(features.nu_03, moments_normalized[3, 0])
+        assert_equal(features.nu_30, moments_normalized[0, 3])
+
+        # Tests hu moments.
+        moments_hu = _convert(props, 'moments_hu')
+        assert_equal(features.hu_1, moments_hu[0])
+        assert_equal(features.hu_2, moments_hu[1])
+        assert_equal(features.hu_3, moments_hu[2])
+        assert_equal(features.hu_4, moments_hu[3])
+        assert_equal(features.hu_5, moments_hu[4])
+
+    def test_feature_extraction(self):
+        segmentation = np.array([[0, 1, 1, 4, 6, 6], [0, 0, 1, 4, 6, 7],
+                                 [0, 3, 1, 5, 5, 7], [0, 2, 2, 2, 5, 7],
+                                 [8, 8, 2, 5, 5, 9], [8, 8, 8, 9, 9, 9]])
+
+        features = FeatureExtraction(segmentation)
+        props = regionprops(segmentation + 1)
+
+        moments_central = _convert(props, 'moments_central')
+        assert_almost_equal(features.mu_11, moments_central[1, 1])
+        assert_almost_equal(features.mu_02, moments_central[2, 0])
+        assert_almost_equal(features.mu_20, moments_central[0, 2])
+        assert_almost_equal(features.mu_12, moments_central[2, 1])
+        assert_almost_equal(features.mu_21, moments_central[1, 2])
+        assert_almost_equal(features.mu_03, moments_central[3, 0])
+        assert_almost_equal(features.mu_30, moments_central[0, 3])
+
+        inertia_tensor = _convert(props, 'inertia_tensor')
+        assert_almost_equal(features.inertia_tensor_02, inertia_tensor[1, 1])
+        assert_almost_equal(features.inertia_tensor_20, inertia_tensor[0, 0])
+        nu_11 = -1 * inertia_tensor[0, 1]
+        assert_almost_equal(features.inertia_tensor_11, nu_11)
+        nu_11 = -1 * inertia_tensor[1, 0]
+        assert_almost_equal(features.inertia_tensor_11, nu_11)
+
+        eigvals = _convert(props, 'inertia_tensor_eigvals')
+        assert_almost_equal(features.inertia_tensor_eigvals_1, eigvals[0])
+        assert_almost_equal(features.inertia_tensor_eigvals_2, eigvals[1])
+
+        moments_normalized = _convert(props, 'moments_normalized')
+        assert_almost_equal(features.nu_11, moments_normalized[1, 1])
+        assert_almost_equal(features.nu_02, moments_normalized[2, 0])
+        assert_almost_equal(features.nu_20, moments_normalized[0, 2])
+        assert_almost_equal(features.nu_12, moments_normalized[2, 1])
+        assert_almost_equal(features.nu_21, moments_normalized[1, 2])
+        assert_almost_equal(features.nu_03, moments_normalized[3, 0])
+        assert_almost_equal(features.nu_30, moments_normalized[0, 3])
+
+        moments_hu = _convert(props, 'moments_hu')
+        assert_almost_equal(features.hu_1, moments_hu[0])
+        assert_almost_equal(features.hu_2, moments_hu[1])
+        assert_almost_equal(features.hu_3, moments_hu[2])
+        assert_almost_equal(features.hu_4, moments_hu[3])
+        assert_almost_equal(features.hu_5, moments_hu[4])
