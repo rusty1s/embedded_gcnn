8a94e15b9fcac8dac3f6afe37eeb063a2567763c
rusty1s <matthias.fey@tu-dortmund.de>
2017-05-05 16:48:59 +0200

    added extract features to segmentation module

--- lib/segmentation/__init__.py
+++ lib/segmentation/__init__.py
@@ -1,13 +1,14 @@
 from .adjacency import segmentation_adjacency
-from .form_feature_extraction import FormFeatureExtraction
+from .feature_extraction import extract_features, extract_features_fixed
 from .form_feature_selection import FormFeatureSelection
 from .algorithm import (slic, slic_fixed, quickshift, quickshift_fixed,
                         felzenszwalb, felzenszwalb_fixed)
 
 __all__ = [
     'segmentation_adjacency',
-    'FormFeatureExtraction',
     'FormFeatureSelection',
+    'extract_features',
+    'extract_features_fixed',
     'slic',
     'slic_fixed',
     'quickshift',
