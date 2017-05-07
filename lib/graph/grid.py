b28a5ad937eaf257ee150dc6c931dcec40cb924c
rusty1s <matthias.fey@tu-dortmund.de>
2017-05-07 18:07:29 +0200

    smarter grid impl

--- lib/graph/grid.py
+++ lib/graph/grid.py
@@ -1,4 +1,7 @@
 import numpy as np
+import scipy.sparse as sp
+
+from .distortion import filter_adj
 
 
 def grid_adj(shape, connectivity=4, dtype=np.float32):
@@ -6,8 +9,32 @@ def grid_adj(shape, connectivity=4, dtype=np.float32):
 
     assert connectivity == 4 or connectivity == 8
 
-    # TODO
-    raise NotImplementedError
+    h, w = shape
+
+    if connectivity == 4:
+        filt = [-w - 2, -1, 1, w + 2]
+    else:
+        filt = [-w - 3, -w - 2, -w - 1, -1, 1, w + 1, w + 2, w + 3]
+
+    # Build basic rows and cols with +1 padding on all sides.
+    n = (h + 1) * (w + 2) - 1
+    rows = np.arange(w + 3, n).repeat(connectivity)
+    rows = np.reshape(rows, (-1, connectivity))
+    cols = rows + filt
+    rows = rows.flatten()
+    cols = cols.flatten()
+
+    data = np.ones_like(rows, dtype=np.uint8)
+    n = (h + 2) * (w + 2)
+    adj = sp.coo_matrix((data, (rows, cols)), (n, n))
+
+    # Compute filter nodes.
+    rows = np.arange(w + 2, h * (w + 3), w + 2).repeat(w)
+    rows = np.reshape(rows, (-1, w))
+    cols = np.arange(1, w + 1)
+    nodes = (rows + cols).flatten()
+
+    return filter_adj(adj, nodes)
 
 
 def grid_points(shape, dtype=np.float32):
--- lib/graph/grid_test.py
+++ lib/graph/grid_test.py
@@ -2,7 +2,7 @@ from unittest import TestCase
 
 from numpy.testing import assert_equal
 
-from .grid import grid_points, grid_mass
+from .grid import grid_adj, grid_points, grid_mass
 
 
 class GridTest(TestCase):
@@ -16,58 +16,16 @@ class GridTest(TestCase):
         expected = [1, 1, 1, 1, 1, 1]
         assert_equal(mass, expected)
 
+    def test_grid_adj_connectivity_4(self):
+        adj = grid_adj((3, 2), connectivity=4)
+        expected = [[0, 1, 1, 0, 0, 0], [1, 0, 0, 1, 0, 0], [1, 0, 0, 1, 1, 0],
+                    [0, 1, 1, 0, 0, 1], [0, 0, 1, 0, 0, 1], [0, 0, 0, 1, 1, 0]]
 
-#     def test_grid_adj_connectivity_8(self):
-#         adj = grid_adj((3, 2), connectivity=8)
+        assert_equal(adj.toarray(), expected)
 
-#       expected = [[0, 1, 1, 2, 0, 0], [1, 0, 2, 1, 0, 0], [1, 2, 0, 1, 1, 2],
-#                   [2, 1, 1, 0, 2, 1], [0, 0, 1, 2, 0, 1], [0, 0, 2, 1, 1, 0]]
+    def test_grid_adj_connectivity_8(self):
+        adj = grid_adj((3, 2), connectivity=8)
+        expected = [[0, 1, 1, 1, 0, 0], [1, 0, 1, 1, 0, 0], [1, 1, 0, 1, 1, 1],
+                    [1, 1, 1, 0, 1, 1], [0, 0, 1, 1, 0, 1], [0, 0, 1, 1, 1, 0]]
 
-#         assert_equal(adj.toarray(), expected)
-# def test_points_to_embedded_grid_4(self):
-#     points = np.array([[2, 2], [2, 3], [3, 2], [2, 1], [1, 2]])
-#     adj = sp.coo_matrix([[0, 1, 1, 1, 1], [1, 0, 0, 0, 0], [1, 0, 0, 0, 0],
-#                          [1, 0, 0, 0, 0], [1, 0, 0, 0, 0]])
-#     adj_dist, adj_rad = points_to_embedded_adj(points, adj)
-
-#     expected_dist = [[0, 1, 1, 1, 1], [1, 0, 0, 0, 0], [1, 0, 0, 0, 0],
-#                      [1, 0, 0, 0, 0], [1, 0, 0, 0, 0]]
-#     expected_rad = [[0, 2 * np.pi, 0.5 * np.pi, np.pi, 1.5 * np.pi],
-#                     [np.pi, 0, 0, 0, 0], [1.5 * np.pi, 0, 0, 0, 0],
-#                     [2 * np.pi, 0, 0, 0, 0], [0.5 * np.pi, 0, 0, 0, 0]]
-
-#     assert_equal(adj_dist.toarray(), expected_dist)
-#     assert_almost_equal(adj_rad.toarray(), expected_rad, decimal=6)
-
-# def test_points_to_embedded_grid_8(self):
-#     points = np.array([[2, 2], [2, 3], [3, 3], [3, 2], [3, 1], [2, 1],
-#                        [1, 1], [1, 2], [1, 3]])
-#     adj = sp.coo_matrix(
-#         [[0, 1, 1, 1, 1, 1, 1, 1, 1], [1, 0, 0, 0, 0, 0, 0, 0, 0],
-#          [1, 0, 0, 0, 0, 0, 0, 0, 0], [1, 0, 0, 0, 0, 0, 0, 0, 0],
-#          [1, 0, 0, 0, 0, 0, 0, 0, 0], [1, 0, 0, 0, 0, 0, 0, 0, 0],
-#          [1, 0, 0, 0, 0, 0, 0, 0, 0], [1, 0, 0, 0, 0, 0, 0, 0, 0],
-#          [1, 0, 0, 0, 0, 0, 0, 0, 0]])
-#     adj_dist, adj_rad = points_to_embedded_adj(points, adj)
-
-#     expected_dist = [
-#         [0, 1, 2, 1, 2, 1, 2, 1, 2], [1, 0, 0, 0, 0, 0, 0, 0, 0],
-#         [2, 0, 0, 0, 0, 0, 0, 0, 0], [1, 0, 0, 0, 0, 0, 0, 0, 0],
-#         [2, 0, 0, 0, 0, 0, 0, 0, 0], [1, 0, 0, 0, 0, 0, 0, 0, 0],
-#         [2, 0, 0, 0, 0, 0, 0, 0, 0], [1, 0, 0, 0, 0, 0, 0, 0, 0],
-#         [2, 0, 0, 0, 0, 0, 0, 0, 0]
-#     ]
-#     expected_rad = [[
-#         0, 2 * np.pi, 0.25 * np.pi, 0.5 * np.pi, 0.75 * np.pi, np.pi,
-#         1.25 * np.pi, 1.5 * np.pi, 1.75 * np.pi
-#     ], [np.pi, 0, 0, 0, 0, 0, 0, 0, 0],
-#                     [1.25 * np.pi, 0, 0, 0, 0, 0, 0, 0, 0],
-#                     [1.5 * np.pi, 0, 0, 0, 0, 0, 0, 0, 0],
-#                     [1.75 * np.pi, 0, 0, 0, 0, 0, 0, 0, 0],
-#                     [2 * np.pi, 0, 0, 0, 0, 0, 0, 0, 0],
-#                     [0.25 * np.pi, 0, 0, 0, 0, 0, 0, 0, 0],
-#                     [0.5 * np.pi, 0, 0, 0, 0, 0, 0, 0, 0],
-#                     [0.75 * np.pi, 0, 0, 0, 0, 0, 0, 0, 0]]
-
-#     assert_equal(adj_dist.toarray(), expected_dist)
-#     assert_almost_equal(adj_rad.toarray(), expected_rad, decimal=6)
+        assert_equal(adj.toarray(), expected)
