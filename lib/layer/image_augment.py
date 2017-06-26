79937f5bdf7350d023852fe92b0213c4bf089f17
rusty1s <matthias.fey@tu-dortmund.de>
2017-06-26 12:03:47 +0200

    added image augmenting as layer

---
+++ lib/layer/image_augment.py
@@ -0,0 +1,20 @@
+import tensorflow as tf
+
+from .layer import Layer
+
+
+class ImageAugment(Layer):
+    def __init__(self, **kwargs):
+        super(ImageAugment, self).__init__(**kwargs)
+
+    def _call(self, inputs):
+        outputs = inputs
+
+        outputs = tf.map_fn(
+            lambda image: tf.image.random_flip_left_right(image), outputs)
+        outputs = tf.map_fn(
+            lambda image: tf.image.random_brightness(image, 0.2), outputs)
+        outputs = tf.map_fn(
+            lambda image: tf.image.random_contrast(image, 0.7, 1.3), outputs)
+        outputs = tf.map_fn(
+            lambda image: tf.image.per_image_standardization(image), outputs)
