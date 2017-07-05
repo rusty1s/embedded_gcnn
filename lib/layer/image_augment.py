fixed standard deviation bug, augmenting fix

lib/layer/image_augment.py
@@ -13,9 +13,9 @@ class ImageAugment(Layer):
         outputs = tf.map_fn(
             lambda image: tf.image.random_flip_left_right(image), outputs)
         outputs = tf.map_fn(
-            lambda image: tf.image.random_brightness(image, 0.2), outputs)
+            lambda image: tf.image.random_brightness(image, 0.3), outputs)
         outputs = tf.map_fn(
-            lambda image: tf.image.random_contrast(image, 0.8, 1.2), outputs)
+            lambda image: tf.image.random_contrast(image, 0.7, 1.3), outputs)
         outputs = tf.map_fn(
             lambda image: tf.image.per_image_standardization(image), outputs)
 

lib/segmentation/feature_extraction.py
@@ -1,14 +1,10 @@
 import numpy as np
 import numpy_groupies as npg
-from sklearn.preprocessing import StandardScaler
 
 from .form_feature_extraction import FormFeatureExtraction
 
 
-def extract_features(segmentation,
-                     image,
-                     form_features=None,
-                     scaler=StandardScaler()):
+def extract_features(segmentation, image, form_features=None):
     features = FormFeatureExtraction(segmentation).get_features(form_features)
 
     group_idx = segmentation.flatten()
@@ -28,12 +24,7 @@ def extract_features(segmentation,
     else:
         raise ValueError
 
-    features = features.astype(np.float32)
-
-    if scaler is None:
-        return features
-    else:
-        return scaler.fit_transform(features)
+    return features.astype(np.float32)
 
 
 def extract_features_fixed(form_features=None):