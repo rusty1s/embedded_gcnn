a4760ffb322e07d2cfa1e948ac8c63629c40652a
rusty1s <matthias.fey@tu-dortmund.de>
2017-07-08 11:32:12 +0200

    better parameters

--- cifar_embedded.py
+++ cifar_embedded.py
@@ -21,7 +21,7 @@ SCALE_INVARIANCE = False
 STDDEV = 1
 
 LEARNING_RATE = 0.001
-NUM_STEPS_PER_DECAY = 1000
+NUM_STEPS_PER_DECAY = 5000
 TRAIN_DIR = None
 # LOG_DIR = 'data/summaries/cifar_slic_embedded'
 LOG_DIR = 'data/summaries/cifar_quickshift_embedded'
@@ -53,49 +53,40 @@ class Model(BaseModel):
     def _build(self):
         conv_1_1 = Conv(
             NUM_FEATURES,
-            32,
+            64,
+            adjs_dist=self.placeholders['adj_dist_1'],
+            adjs_rad=self.placeholders['adj_rad_1'],
+            logging=self.logging)
+        conv_1_2 = Conv(
+            64,
+            64,
             adjs_dist=self.placeholders['adj_dist_1'],
             adjs_rad=self.placeholders['adj_rad_1'],
             logging=self.logging)
         max_pool_1 = MaxPool(size=2)
-        conv_2_1 = Conv(
-            32,
+        conv_2 = Conv(
             64,
+            128,
             adjs_dist=self.placeholders['adj_dist_2'],
             adjs_rad=self.placeholders['adj_rad_2'],
             logging=self.logging)
         max_pool_2 = MaxPool(size=2)
-        conv_3_1 = Conv(
-            64,
+        conv_3 = Conv(
             128,
+            256,
             adjs_dist=self.placeholders['adj_dist_3'],
             adjs_rad=self.placeholders['adj_rad_3'],
             logging=self.logging)
         max_pool_3 = MaxPool(size=2)
-        conv_4_1 = Conv(
-            128,
-            256,
-            adjs_dist=self.placeholders['adj_dist_4'],
-            adjs_rad=self.placeholders['adj_rad_4'],
-            logging=self.logging)
-        conv_4_2 = Conv(
+        conv_4 = Conv(
             256,
             512,
             adjs_dist=self.placeholders['adj_dist_4'],
             adjs_rad=self.placeholders['adj_rad_4'],
             logging=self.logging)
-        max_pool_4 = MaxPool(size=2)
         average_pool = AveragePool()
-        fc_1 = FC(
-            512,
-            256,
-            dropout=self.placeholders['dropout'],
-            logging=self.logging)
-        fc_2 = FC(
-            256,
-            128,
-            dropout=self.placeholders['dropout'],
-            logging=self.logging)
+        fc_1 = FC(512, 256, weight_decay=0.004, logging=self.logging)
+        fc_2 = FC(256, 128, weight_decay=0.004, logging=self.logging)
         fc_3 = FC(
             128,
             data.num_classes,
@@ -105,8 +96,8 @@ class Model(BaseModel):
             logging=self.logging)
 
         self.layers = [
-            conv_1_1, max_pool_1, conv_2_1, max_pool_2, conv_3_1, max_pool_3,
-            conv_4_1, conv_4_2, max_pool_4, average_pool, fc_1, fc_2, fc_3
+            conv_1_1, conv_1_2, max_pool_1, conv_2, max_pool_2, conv_3,
+            max_pool_3, conv_4, average_pool, fc_1, fc_2, fc_3
         ]
 
 
