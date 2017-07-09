210bcb21a7eb529d230b02fac3e41558c220a51f
rusty1s <matthias.fey@tu-dortmund.de>
2017-07-09 12:06:30 +0200

    added spatial methods to init

--- lib/graph/__init__.py
+++ lib/graph/__init__.py
@@ -3,6 +3,7 @@ from .distortion import (perm_features, filter_adj, filter_features,
                          gray_color_threshold, degree_threshold,
                          area_threshold)
 from .grid import grid_adj, grid_points, grid_mass
+from .spatial import receptive_fields, fill_features
 
 __all__ = [
     'coarsen_adj',
@@ -15,4 +16,6 @@ __all__ = [
     'grid_adj',
     'grid_points',
     'grid_mass',
+    'receptive_fields',
+    'fill_features',
 ]
