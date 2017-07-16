398258ac7558307b0d0f851ac52d18088223be84
rusty1s <matthias.fey@tu-dortmund.de>
2017-07-16 16:44:58 +0200

    removed tiny image net

--- lib/datasets/__init__.py
+++ lib/datasets/__init__.py
@@ -1,7 +1,6 @@
 from .mnist import MNIST
 from .cifar_10 import Cifar10
-from .tiny_image_net import TinyImageNet
 from .pascal_voc import PascalVOC
 from .queue import PreprocessQueue
 
-__all__ = ['MNIST', 'Cifar10', 'TinyImageNet', 'PascalVOC', 'PreprocessQueue']
+__all__ = ['MNIST', 'Cifar10', 'PascalVOC', 'PreprocessQueue']
