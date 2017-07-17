ccf7a7735b9b7b12ff403dcc2cb9572dbd5ad3b2
rusty1s <matthias.fey@tu-dortmund.de>
2017-07-17 23:25:53 +0200

    typo

--- pascal_graph.py
+++ pascal_graph.py
@@ -7,7 +7,7 @@ from lib.pipeline import preprocess_pipeline_fixed
 from lib.layer import EmbeddedGCNN as Conv, MaxPool, AveragePool, FC
 
 SLIC_FORM_FEATURES = [1, 2, 3, 4, 5, 6, 8, 9, 21]
-# QUICKSIFT_FORM_FEATURES = [0, 2, 3, 4, 7, 19, 20, 21, 22]
+# QUICKSHIFT_FORM_FEATURES = [0, 2, 3, 4, 7, 19, 20, 21, 22]
 
 DATA_DIR = 'data/pascal_voc'
 PREPROCESS_FIRST = 'data/pascal_voc/slic'
