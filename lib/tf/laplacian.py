523d275650648fc9a5235da6fd892a699cab2705
rusty1s <matthias.fey@tu-dortmund.de>
2017-07-17 12:12:31 +0200

    comments

--- lib/tf/laplacian.py
+++ lib/tf/laplacian.py
@@ -4,6 +4,9 @@ from .math import sparse_tensor_diag_matmul, sparse_scalar_multiply
 
 
 def rescaled_laplacian(adj):
+    """Creates a tensorflow (rescale) laplacian matrix out of a
+    SparseTensorValue adjacency matrix."""
+
     degree = tf.sparse_reduce_sum(adj, axis=1)
     degree = tf.cast(degree, tf.float32)
     degree = tf.pow(degree, -0.5)
--- lib/tf/math.py
+++ lib/tf/math.py
@@ -3,11 +3,16 @@ import tensorflow as tf
 
 
 def sparse_scalar_multiply(value, scalar):
+    """Multiply a SparseTensorValue by a scalar."""
+
     return tf.SparseTensorValue(value.indices, scalar * value.values,
                                 value.dense_shape)
 
 
 def sparse_tensor_diag_matmul(a, diag, transpose=False):
+    """Multiply a SparseTensorValue with a diagonal matrix given its diagonal
+    values."""
+
     _py_func = _diag_matmul_py if not transpose else _diag_matmul_transpose_py
     values = tf.py_func(
         _py_func, [a.indices, a.values, diag], Tout=diag.dtype, stateful=False)
